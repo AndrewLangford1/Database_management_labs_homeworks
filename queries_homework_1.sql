22cecf2e384ce68c5bcd18b881ad12f0578673e1
Andrew <alangford103@hotmail.com>
2013-09-15 20:25:49 -0400

    Questions 9 and 10

--- queries_homework_1.sql
+++ queries_homework_1.sql
@@ -2,46 +2,55 @@
 --Queries Homework 1 --
 
 
---Lists all data for all customers.--
+--1. Lists all data for all customers.--
 select * 
 from   customers;
 
---Lists the name and city of agents named Smith.--
+--2. Lists the name and city of agents named Smith.--
 select 	name,city 
 from   	agents 
 where	name = 'Smith';
 
 
---Lists pid, name, and quantity of products costing more than US$1.25.--
+--3. Lists pid, name, and quantity of products costing more than US$1.25.--
 select 	pid,name,quantity 
 from 	Products 
 where 	priceusd > 1.25;
 
---Lists the ordno and aid of all orders.--
+--4. Lists the ordno and aid of all orders.--
 select 	ordno,aid 
 from 	Orders;
 
---Lists the names and cities of customers not in Dallas.--
+--5. Lists the names and cities of customers not in Dallas.--
 select	name,city 
 from 	Customers 
 where 	city <> 'Dallas';
 
---Lists the names of agents in New York or Newark.-- 
+--6. Lists the names of agents in New York or Newark.-- 
 select 	name
 from	Agents
 where 	city = 'New York'
 or 		city = 'Newark';
 
---Lists all data for products not in New York or Newark that cost US$1 or less.--
+--7. Lists all data for products not in New York or Newark that cost US$1 or less.--
 select 	*
 from	Products
 where 	priceusd <= 1.00
 and		(city <> 'Newark' and city <> 'New York');
 
---Lists all data for orders in January or March. --
+--8. Lists all data for orders in January or March. --
 select	*
 from 	Orders
 where 	mon = 'jan'
 or 		mon = 'mar';
 
+--9. Lists all data for orders in February less than US$100. --
+select	*
+from	Orders
+where	mon = 'feb'
+and 	dollars < 100.00;
 
+--10. Lists all orders from the customer whose cid is C005.--
+select	*
+from 	Orders
+where 	cid = 'c005';
