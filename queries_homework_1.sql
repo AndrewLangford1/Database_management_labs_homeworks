7a0a05a2cdda5bdf83ea31f9bdb6ea5e4da8df86
Andrew <alangford103@hotmail.com>
2013-09-15 20:20:22 -0400

    Added and fixed comments. Question 8.

--- queries_homework_1.sql
+++ queries_homework_1.sql
@@ -2,42 +2,46 @@
 --Queries Homework 1 --
 
 
---Displays all data about customers in Customers table.--
+--Lists all data for all customers.--
 select * 
 from   customers;
 
---Displays the name and city for agents with name 'Smith' in Agents table.--
+--Lists the name and city of agents named Smith.--
 select 	name,city 
 from   	agents 
 where	name = 'Smith';
 
 
---Lists pid,name, and quantity of any products with a price greater than $1.25--
+--Lists pid, name, and quantity of products costing more than US$1.25.--
 select 	pid,name,quantity 
 from 	Products 
 where 	priceusd > 1.25;
 
---Lists ordno and aid for all orders in Orders table.--
+--Lists the ordno and aid of all orders.--
 select 	ordno,aid 
 from 	Orders;
 
---Lists the names and cities of customers in Customers table not in Dallas.--
+--Lists the names and cities of customers not in Dallas.--
 select	name,city 
 from 	Customers 
 where 	city <> 'Dallas';
 
---Lists the names of agents in Agents table located in New York or Newark-- 
+--Lists the names of agents in New York or Newark.-- 
 select 	name
 from	Agents
 where 	city = 'New York'
 or 		city = 'Newark';
 
---Lists all data about products in Products whose cost is $1 or less and isn't from Newark or New York--
+--Lists all data for products not in New York or Newark that cost US$1 or less.--
 select 	*
 from	Products
 where 	priceusd <= 1.00
 and		(city <> 'Newark' and city <> 'New York');
 
-
+--Lists all data for orders in January or March. --
+select	*
+from 	Orders
+where 	mon = 'jan'
+or 		mon = 'mar';
 
 
