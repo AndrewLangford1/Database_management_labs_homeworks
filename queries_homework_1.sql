77232a6ee408c57a04c2fe72f0a90dc411a90381
Andrew <alangford103@hotmail.com>
2013-09-15 20:13:48 -0400

    questions 6 and 7

--- queries_homework_1.sql
+++ queries_homework_1.sql
@@ -17,12 +17,27 @@ select 	pid,name,quantity
 from 	Products 
 where 	priceusd > 1.25;
 
---Lists ordno and aid for all orders.--
+--Lists ordno and aid for all orders in Orders table.--
 select 	ordno,aid 
 from 	Orders;
 
+--Lists the names and cities of customers in Customers table not in Dallas.--
 select	name,city 
 from 	Customers 
 where 	city <> 'Dallas';
+
+--Lists the names of agents in Agents table located in New York or Newark-- 
+select 	name
+from	Agents
+where 	city = 'New York'
+or 		city = 'Newark';
+
+--Lists all data about products in Products whose cost is $1 or less and isn't from Newark or New York--
+select 	*
+from	Products
+where 	priceusd <= 1.00
+and		(city <> 'Newark' and city <> 'New York');
+
+
 
 
