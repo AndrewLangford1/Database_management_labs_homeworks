feed077e4937f1dec04b91d9c71b86a75cddbc8e
AndrewLangford1 <andrew.langford92@gmail.com>
2013-10-01 17:07:50 -0400

    progress on questions 3 and 4, completed question 5 (sql homework 3)

--- queries_homework_3.sql
+++ queries_homework_3.sql
@@ -17,3 +17,27 @@ select distinct city
 from agents a, orders o
 where a.aid = o.aid
 and o.cid = 'c002';
+
+--Question 3 --
+-- 3. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto. Use subqueries. (Yes, this is also the same question as on homework #2.) --
+select pid
+from products
+where pid in (select pid
+			  from orders
+			  where aid in (select aid
+			  				from agents
+			  				where city = 'Kyoto'));
+
+
+
+-- Question 4 --
+-- 4. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto. Use joins thus time; no subqueries. --
+
+
+
+-- Question 5 --
+-- Get the names of customers who have never placed an order. Use a subquery.--
+select name
+from customers
+where cid not in (select cid
+				  from orders);
